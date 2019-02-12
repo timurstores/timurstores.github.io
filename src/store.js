140b6d836de092bca04e8fb2e8850b4bbb23a6f6
timurstores <46571119+timurstores@users.noreply.github.com>
2019-02-12 16:42:35 +0600

    Update store.js

--- src/store.js
+++ src/store.js
@@ -1,8 +1,8 @@
 // import the ability to modify browser history within our router
 import createHistory from 'history/createBrowserHistory';
 
-// import our logger for redux
-import { createLogger } from 'redux-logger';
+const createLogger = require(`redux-logger`);
+ngRedux.configureStore(rootReducer, {}, [createLogger({collapsed: 'true'})]);
 
 // import a library to handle async with redux
 import thunk from 'redux-thunk';
